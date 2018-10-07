51e913033ed71a90c847a43a06ff11da0c9ddf10
May Chen <maychen@Mays-MacBook-Pro.local>
2018-10-06 19:41:24 -1000

    Initial commit. Make sanity check work for Queue, In Progress, Done, and Card components

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="Card">
+        <header className="Card-header">
+          <h1 className="columnTitle">Card</h1>
+        </header>
+      </div>
+    )
+  }
+
+}
+
+export default Card;
